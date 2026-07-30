d5db653f70712085751bff003ffc71d354f01dba
mmiskatul <masabimiskat@gmail.com>
2026-07-30 19:12:45 +0600

    feat: add happy hours navigation tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -128,6 +128,19 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+        name="happy-hours"
+        options={{
+          title: "Happy Hour",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "pricetag" : "pricetag-outline"}
+              size={25}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
         name="save"
         options={{
           title: "Save",
